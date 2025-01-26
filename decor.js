6d9724dff2eb196642f1daa806021aa035ba26ef
Dan MacKinnon <dmackinnon1@gmail.com>
2025-01-26 15:12:59 -0500

    ch 3 exp

--- decor.js
+++ decor.js
@@ -119,9 +119,11 @@ for (let d=0; d < allForegrounds.length; d++ ){
 	});
 
 	ch3Doc.section("Design using " + foreground + " and " + background);
-	ch3Doc.addContent(new doc.RawText("\\marginnote{\\centering\\input{"+foreFile+"}\n" +foreground + "}"));
-	ch3Doc.addContent(new doc.RawText("\\marginnote{\\centering\\input{"+backFile+"}\n" +background + "}"));
+	ch3Doc.addContent(new doc.RawText("\\marginnote{\\centering\\input{"+foreFile+"}\\\ \n" +foreground + "}"));
+	ch3Doc.addContent(new doc.RawText("\\marginnote{\\centering\\input{"+backFile+"}\\\ \n" +background + "}"));
+	ch3Doc.addContent(new doc.RawText("\n \\begin{center}\n"));
 	ch3Doc.input(designFile);
+	ch3Doc.addContent(new doc.RawText("\n \\end{center}\n"));
 	ch3Doc.addContent(new doc.RawText("\n"))
 			.command("vspace","0.2cm",true);	
 }
